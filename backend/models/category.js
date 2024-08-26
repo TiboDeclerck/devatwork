b1c55b91ee321aba2eac050283bd9980b91b4cc7
TiboDeclerck <tibo6c04@gmail.com>
2024-08-26 21:18:10 +0200

    feat(models): added timestamps to Category and Supplier

--- backend/models/category.js
+++ backend/models/category.js
@@ -1,10 +1,14 @@
 export default class Category {
-  constructor({ id, name }) {
+  constructor({ id, name,
+    createdAt = new Date(),
+    updatedAt = new Date(), }) {
     if (!name) {
       throw new Error("Category name is required");
     }
 
     this.id = id;
     this.name = name;
+    this.createdAt = createdAt;
+    this.updatedAt = updatedAt;
   }
 }
--- backend/models/supplier.js
+++ backend/models/supplier.js
@@ -1,11 +1,12 @@
 export default class Supplier {
-    constructor({ id, name }) {
-      if (!name) {
-        throw new Error("Supplier name is required");
-      }
-  
-      this.id = id;
-      this.name = name;
+  constructor({ id, name, createdAt = new Date(), updatedAt = new Date() }) {
+    if (!name) {
+      throw new Error("Supplier name is required");
     }
+
+    this.id = id;
+    this.name = name;
+    this.createdAt = createdAt;
+    this.updatedAt = updatedAt;
   }
-  
+}
